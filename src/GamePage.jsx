0a90471708a0f77b311ab7ab79f8ded3fa24cb4e
SaadHabashneh <Saad.K.Habashneh@gmail.com>
2024-04-30 11:58:39 +0300

    created the function that distributes the values into the cards

--- src/GamePage.jsx
+++ src/GamePage.jsx
@@ -1,39 +1,33 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 const GamePage = () => {
+  const [cards, setCards] = useState();
 
-    const [cards, setCards] = useState();
+  const cardGen = () => {
+    const arr = [];
+    const vals = ["A", "B", "C", "D", "E", "F", "G", "H"];
+    const counts = {};
+    for (let i = 0; i < 16; i++) {
+      let val = vals[Math.floor(Math.random() * vals.length)];
+      counts[val] = (counts[val] || 0) + 1;
+      if (counts[val] == 2) {
+        vals.splice(vals.indexOf(val), 1);
+      }
+      arr.push({ id: i + 1, value: val });
+    }
+    setCards(arr);
+  };
 
-    const cardGen = () => {
-        const arr = [];
-        const vals = ["A", "B", "C", "D", "E", "F", "G", "H"];
-        let val = "";
-        let vNum = 0;
-        for (let i = 1; i <= 16; i++) {
-            arr.push({id: i, value: vals[Math.floor(Math.random() * vals.length)]});
-            val = arr[i - 1].value;
-            for (let j = 0; j < arr.length; j++) {
-                if (arr[j].value == val) {
-                    vNum++;
-                }
-                if (vNum == 2) {
-                    vals.splice(vals.indexOf(val), 1);
-                }
-            }
-        }
-        setCards(arr);
-    };
-
-    useEffect(cardGen, []);
+  useEffect(cardGen, []);
 
   return (
-    <div className='gameDiv'>
-        {/* {cards && (cards.map(card => (
-            
-        )))} */}
+    <div className="gameDiv">
+      {cards && cards.map(card => {
+        <button value={card.value}>{card.id}</button>
+      })}
     </div>
-  )
+  );
 };
 
 export default GamePage;
