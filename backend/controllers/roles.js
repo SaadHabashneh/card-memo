e951e6fa722b44086c8cfb1b22fa458ea6c6ca94
SaadHabashneh <Saad.K.Habashneh@gmail.com>
2024-05-13 14:14:08 +0300

    fixed and formatted the createRole controller

--- backend/controllers/roles.js
+++ backend/controllers/roles.js
@@ -1,24 +1,27 @@
 const pool = require("../models/db");
 
 const createRole = (req, res) => {
-    const {role} = req.body;
-    const query = `INSERT INTO roles VALUES ($1) RETURNING *;`;
-    const data = [role];
-    pool.query((query, data).then((result) => {
-        res.status(201).json({
-            success: true,
-            message: "role created successfully",
-            role: result.rows
-        });
-    }).catch((err) => {
-        res.status(500).json({
-            success: false,
-            message: "Server error",
-            error: err.message
-        });
-    }));
+  const { role } = req.body;
+  const query = `INSERT INTO roles (role) VALUES ($1) RETURNING *;`;
+  const data = [role];
+  pool
+    .query(query, data)
+    .then((result) => {
+      res.status(201).json({
+        success: true,
+        message: "role created successfully",
+        role: result.rows,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: "Server error",
+        error: err.message,
+      });
+    });
 };
 
 module.exports = {
-    createRole
+  createRole,
 };
