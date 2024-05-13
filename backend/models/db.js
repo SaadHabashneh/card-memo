8e3500c3d35b5369728924958803a9b5c2ebb914
SaadHabashneh <Saad.K.Habashneh@gmail.com>
2024-05-13 11:03:42 +0300

    created the database connection in the db.js file

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,10 +1,20 @@
 const mysql = require("mysql");
 
 const pool = mysql.createPool({
-    host: '',
-    user: '',
+    host: 'localhost',
+    user: 'root',
     password: '',
-    database: ''
+    database: 'card-memo'
+});
+
+pool.getConnection((err, connection) => {
+    if (err) {
+        console.log('Error connecting to the database:', err);
+    }
+    else {
+        console.log('Database is connected');
+        connection.release();
+    }
 });
 
 module.exports = pool;
