ce7debd7fb9220955bb36c7ab9bdadae6303c29c
SaadHabashneh <Saad.K.Habashneh@gmail.com>
2024-05-16 14:11:15 +0300

    created the permissions table

--- backend/models/db.js
+++ backend/models/db.js
@@ -2,28 +2,29 @@ const mysql = require("mysql2/promise");
 const fs = require("fs");
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'card-memo'
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "card-memo",
 });
 
 pool.getConnection((err, connection) => {
-    if (err) {
-        console.log('Error connecting to the database:', err);
-    }
-    console.log('Database is connected');
-    connection.release();
+  if (err) {
+    console.log("Error connecting to the database:", err);
+  }
+  console.log("Database is connected");
+  connection.release();
 });
 
-const sql = fs.readFileSync('./models/database.sql').toString();
+const sql = fs.readFileSync("./models/database.sql").toString();
 
-// pool.execute(sql)
-//     .then(() => {
-//         console.log('SQL script performed successfully');
-//     })
-//     .catch((err) => {
-//         console.error('Error executing the SQL script:', err);
-//     });
+pool
+  .execute(sql)
+  .then(() => {
+    console.log("SQL script executed successfully");
+  })
+  .catch((err) => {
+    console.error("Error executing the SQL script:", err);
+  });
 
 module.exports = pool;
