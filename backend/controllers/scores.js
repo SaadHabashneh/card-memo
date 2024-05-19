5bc1ac2a5c299a8093882f155fa68c8798303458
SaadHabashneh <Saad.K.Habashneh@gmail.com>
2024-05-19 13:50:07 +0300

    created the deleteScoreById controller and gave it a router

--- backend/controllers/scores.js
+++ backend/controllers/scores.js
@@ -23,23 +23,72 @@ const addScore = (req, res) => {
 };
 
 const getAllScores = (req, res) => {
-    const query = `SELECT (username), (score) FROM users JOIN scores WHERE users.id = scores.user_id;`;
-    pool.query(query).then((result) => {
-        res.status(200).json({
-            success: true,
-            message: "All scores",
-            scores: result[0]
-        });
-    }).catch((err) => {
-        res.status(500).json({
-            success: false,
-            message: "Server error",
-            error: err.message
-        });
+  const query = `SELECT (username), (score) FROM users JOIN scores WHERE users.id = scores.user_id;`;
+  pool
+    .query(query)
+    .then((result) => {
+      res.status(200).json({
+        success: true,
+        message: "All scores",
+        scores: result[0],
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: "Server error",
+        error: err.message,
+      });
+    });
+};
+
+const getUserScores = (req, res) => {
+  const { id } = req.params;
+  const query = `SELECT * FROM scores WHERE user_id = (?);`;
+  const data = [id];
+  pool
+    .query(query, data)
+    .then((result) => {
+      res.status(200).json({
+        success: true,
+        message: "All your scores",
+        scores: result[0],
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: "Server error",
+        error: err.message,
+      });
+    });
+};
+
+const deleteScoreById = (req, res) => {
+  const { id } = req.params;
+  const query = `DELETE FROM scores WHERE id = (?);`;
+  const data = id;
+  pool
+    .query(query, data)
+    .then((result) => {
+      res.status(200).json({
+        success: true,
+        message: "Score deleted successfully",
+        score: result[0],
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: "Server error",
+        error: err.message,
+      });
     });
 };
 
 module.exports = {
   addScore,
   getAllScores,
+  getUserScores,
+  deleteScoreById,
 };
--- backend/routes/scores.js
+++ backend/routes/scores.js
@@ -1,10 +1,12 @@
 const express = require("express");
 
-const { addScore, getAllScores } = require("../controllers/scores");
+const { addScore, getAllScores, getUserScores, deleteScoreById } = require("../controllers/scores");
 
 const scoresRouter = express.Router();
 
 scoresRouter.post("", addScore);
 scoresRouter.get("", getAllScores);
+scoresRouter.get("/:id", getUserScores);
+scoresRouter.delete("/:id", deleteScoreById);
 
 module.exports = scoresRouter;
