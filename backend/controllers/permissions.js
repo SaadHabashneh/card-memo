9f31475b38fe1c8d002ee33832ef014f24598926
SaadHabashneh <Saad.K.Habashneh@gmail.com>
2024-05-19 11:22:09 +0300

    fixed the addPermission controller

--- backend/controllers/permissions.js
+++ backend/controllers/permissions.js
@@ -2,7 +2,7 @@ const pool = require("../models/db");
 
 const addPermission = (req, res) => {
   const { permission } = req.body;
-  const query = `INSERT INTO permissions (permission) VALUES ?`;
+  const query = `INSERT INTO permissions (permission) VALUES (?)`;
   const data = [permission];
   pool
     .query(query, data)
