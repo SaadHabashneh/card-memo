2c6c204083712bc60536776b1ca98ea650312652
SaadHabashneh <Saad.K.Habashneh@gmail.com>
2024-05-19 10:43:22 +0300

    created scores table

--- backend/models/db.sql
+++ backend/models/db.sql
@@ -3,16 +3,6 @@ CREATE TABLE roles (
     role VARCHAR(255) UNIQUE NOT NULL
 );
 
-CREATE TABLE users (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    role_id BIGINT NOT NULL,
-    FOREIGN KEY (role_id) REFERENCES roles(id),
-    is_deleted SMALLINT DEFAULT 0
-);
-
 CREATE TABLE permissions (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     permission VARCHAR(255) UNIQUE NOT NULL
@@ -24,4 +14,21 @@ CREATE TABLE role_permission (
     permission_id BIGINT,
     FOREIGN KEY (role_id) REFERENCES roles(id),
     FOREIGN KEY (permission_id) REFERENCES permissions(id)
+);
+
+CREATE TABLE users (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role_id BIGINT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    is_deleted SMALLINT DEFAULT 0
+);
+
+CREATE TABLE scores (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    score BIGINT NOT NULL,
+    user_id BIGINT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
