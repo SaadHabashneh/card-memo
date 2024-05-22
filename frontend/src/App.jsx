802ef1056ede6c2dc20942942b7b05c17a382e59
SaadHabashneh <Saad.K.Habashneh@gmail.com>
2024-05-22 11:08:12 +0300

    installed mui packages, created context file for delivering the token of the logged in user around the app, designed the navbar and its buttons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,28 @@
+import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom'; 
 import './App.css';
 import NavBar from './NavBar';
 import LandingPage from './LandingPage';
 import GamePage from './GamePage';
+import Register from './Register';
+import Login from './Login';
+import { AppContext } from "./Context";
+
 
 const App = () => {
-
+  const [token, setToken] = useState(localStorage.getItem("token") || null);
 
   return (
     <div>
+      <AppContext.Provider value={{token, setToken}}>
       <NavBar />
       <Routes>
         <Route path='/' element={LandingPage}></Route>
-        <Route path='/game' element={GamePage}></Route>
+        <Route path='/Game' element={GamePage}></Route>
+        <Route path='/Register' element={Register}></Route>
+        <Route path='/Login' element={Login}></Route>
       </Routes>
+      </AppContext.Provider>
     </div>
   )
 };
--- frontend/src/NavBar.jsx
+++ frontend/src/NavBar.jsx
@@ -1,15 +1,53 @@
-import {useState} from 'react';
-import { Link } from 'react-router-dom';
+import { useContext } from "react";
+import {AppContext} from "./Context";
+import { useNavigate } from "react-router-dom";
 import "./App.css";
+import { AppBar, Toolbar, IconButton, Button } from "@mui/material";
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+
 
 const NavBar = () => {
+  const {token} = useContext(AppContext);
+  const {setToken} = useContext(AppContext);
+  const navigate = useNavigate();
+
+  const logOut = () => {
+    localStorage.removeItem("token");
+    setToken(null);
+    navigate("/");
+  };
+
   return (
-    <div className='nav'>
-        <Link to="/" className='navBtn'>Home</Link>
-        <Link to="/register" className='navBtn'>Register</Link>
-        <Link to="/login" className='navBtn'>Login</Link>
-    </div>
-  )
+    <AppBar position="static">
+      <Toolbar variant="dense" className="navbar">
+        <div>
+        <IconButton
+          className="navBtn"
+          onClick={() => navigate("/")}
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <HomeRoundedIcon></HomeRoundedIcon>
+        </IconButton>
+        </div>
+        <div>
+        <Button className="navBtn" color="inherit" onClick={() => navigate("/AllScores")}>All Scores</Button>
+        <Button className="navBtn" color="inherit" onClick={() => navigate("/Register")}>Register</Button>
+        {token ? (
+          <Button className="navBtn" color="inherit" onClick={logOut}>
+            Logout
+          </Button>
+        ) : (
+          <Button className="navBtn" color="inherit" onClick={() => navigate("/Login")}>
+            Login
+          </Button>
+        )}
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default NavBar;
