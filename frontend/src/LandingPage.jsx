2148ec53fedb1eb1e7ad8f98cd2b072d8415dac0
SaadHabashneh <Saad.K.Habashneh@gmail.com>
2024-05-22 11:28:20 +0300

    added a button to the navbar

--- frontend/src/LandingPage.jsx
+++ frontend/src/LandingPage.jsx
@@ -1,9 +1,10 @@
-import {useState} from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./App.css";
 
 const LandingPage = () => {
-    
+  
+    const navigate = useNavigate();
+
   return (
     <div className="landing">
         <h2 className="welcome">Press start game to play!</h2>
--- frontend/src/NavBar.jsx
+++ frontend/src/NavBar.jsx
@@ -1,14 +1,13 @@
 import { useContext } from "react";
-import {AppContext} from "./Context";
+import { AppContext } from "./Context";
 import { useNavigate } from "react-router-dom";
 import "./App.css";
 import { AppBar, Toolbar, IconButton, Button } from "@mui/material";
-import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
-
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 
 const NavBar = () => {
-  const {token} = useContext(AppContext);
-  const {setToken} = useContext(AppContext);
+  const { token } = useContext(AppContext);
+  const { setToken } = useContext(AppContext);
   const navigate = useNavigate();
 
   const logOut = () => {
@@ -21,29 +20,56 @@ const NavBar = () => {
     <AppBar position="static">
       <Toolbar variant="dense" className="navbar">
         <div>
-        <IconButton
-          className="navBtn"
-          onClick={() => navigate("/")}
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}
-        >
-          <HomeRoundedIcon></HomeRoundedIcon>
-        </IconButton>
+          <IconButton
+            className="navBtn"
+            onClick={() => navigate("/")}
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <HomeRoundedIcon></HomeRoundedIcon>
+          </IconButton>
         </div>
         <div>
-        <Button className="navBtn" color="inherit" onClick={() => navigate("/AllScores")}>All Scores</Button>
-        <Button className="navBtn" color="inherit" onClick={() => navigate("/Register")}>Register</Button>
-        {token ? (
-          <Button className="navBtn" color="inherit" onClick={logOut}>
-            Logout
+          <Button
+            className="navBtn"
+            color="inherit"
+            onClick={() => navigate("/AllScores")}
+          >
+            All Scores
           </Button>
-        ) : (
-          <Button className="navBtn" color="inherit" onClick={() => navigate("/Login")}>
-            Login
-          </Button>
-        )}
+          {token ? (
+            <>
+              <Button
+                className="navBtn"
+                color="inherit"
+                onClick={() => navigate()}
+              >
+                My Scores
+              </Button>
+              <Button className="navBtn" color="inherit" onClick={logOut}>
+                Logout
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button
+                className="navBtn"
+                color="inherit"
+                onClick={() => navigate("/Register")}
+              >
+                Register
+              </Button>
+              <Button
+                className="navBtn"
+                color="inherit"
+                onClick={() => navigate("/Login")}
+              >
+                Login
+              </Button>
+            </>
+          )}
         </div>
       </Toolbar>
     </AppBar>
